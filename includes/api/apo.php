8ad31c17bde7e23dde9cc491d9457d968af3694d
waoneon1 <dhardi@WD-1.indosoft.dc>
2017-01-17 17:26:57 +0700

    dev api

---
+++ includes/api/apo.php
@@ -0,0 +1,21 @@
+<?php
+
+$ktp = array(
+    'KTP' => array(
+        "NIK"   => "0000000000000000",
+        "NO_KK" => "0000000000000000",
+        "NAMA_LGKP" => "xxx",
+        "ALAMAT" => "xxx",
+        "NO_RT" => "1",
+        "NO_RW" => "7",
+        "TMPT_LHR" => "BLITAR",
+        "TGL_LHR" => "11-OCT85",
+        "STAT_KWN" => "1",
+        "AGAMA" => "1",
+        "JENIS_PKRJN" => "15",
+        "NO_PROP" => "35",
+        "NO_KAB" => "73 ",
+        "NO_KEC" => "3",
+        "NO_KEL" => "1009"
+    ),
+);
